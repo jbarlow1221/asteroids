5de4d8f76349c4dcbd27ba7a32aba4ebcd4c85f4
jbarlow1221 <thejordabarlow@gmail.com>
2024-09-24 12:33:47 -0700

    baby steps

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+from constants import *
+def main():
+	pygame.init
+	pygame.init()
+	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	black = (0, 0, 0)
+	while True:
+		for event in pygame.event.get():
+    			if event.type == pygame.QUIT:
+        			return
+		screen.fill(black, rect = None, special_flags = 0)
+		pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
