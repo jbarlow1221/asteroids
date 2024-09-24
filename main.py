5f11b2ab612bfcbb81018bf059815bc4d7396ee1
jbarlow1221 <thejordabarlow@gmail.com>
2024-09-24 15:31:09 -0700

    TRIANGLE

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 def main():
-	pygame.init
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	black = (0, 0, 0)
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
-		screen.fill(black, rect = None, special_flags = 0)
+		screen.fill(black)
+		player.draw(screen)
 		pygame.display.flip()
-
-
-
-
-
+		dt = clock.tick(60) / 1000
 
 
 
